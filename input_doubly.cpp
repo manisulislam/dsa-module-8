e045cc4ef53dc05a6b69d0aafdf602d6f9093f3e
anisul islam <anisulislam980340@gmail.com>
2023-06-18 11:33:16 +0600

    doubly singly list comleted

---
+++ input_doubly.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+     int val;
+     Node* next;
+     Node * prev;
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+
+    }
+};
+void insert_tail(Node*&head, Node*& tail, int val){
+    Node * newNode= new Node(val);
+    if(tail==NULL){
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    tail->next= newNode;
+    newNode->prev=tail;
+    tail=newNode;
+}
+void print_linked_list(Node * head){
+    Node *tmp=head;
+    while(tmp!=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    cout<<endl;
+}
+void print_linked_list_reverse(Node *tail){
+    Node * tmp= tail;
+    while(tmp!=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->prev;
+    }
+    cout<<endl;
+    
+}
+int main(){
+    Node * head = NULL;
+    Node * tail = NULL;
+    int val;
+    while(true){
+        cin>>val;
+        if(val==-1) break;
+        insert_tail(head, tail, val);
+    }
+    
+    print_linked_list(head);
+    print_linked_list_reverse(tail);
+    
+    return 0;
+}
